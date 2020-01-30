4f821e728eeb6f270a8d6c1ab6c0bcdb9d67529e
cjp34 <carlparm@gmail.com>
2020-01-30 17:44:09 -0500

    Final

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,32 @@
-class Doctors
+require 'pry'
+class Doctor
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments 
+        Appointment.all.select do |x|
+            x.doctor == self
+        end
+    end 
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        Appointment.all.select do |x|
+            x.doctor == self
+        end.collect do |y|
+            y.patient
+        end
+    end
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Patient
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments 
+        Appointment.all.select do |x|
+            x.patient == self
+        end
+    end 
+
+    def doctors
+        Appointment.all.select do |x|
+            x.patient == self
+        end.collect do |y|
+            y.doctor
+        end
+    end
 
 end
